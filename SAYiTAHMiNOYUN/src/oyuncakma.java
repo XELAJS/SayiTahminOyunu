c85c7a52dc0dd4f41d594c5dc8c4c071566c6c6b
XELAJS <114605289+XELAJS@users.noreply.github.com>
2022-11-10 14:33:23 +0300

    Add files via upload

---
+++ SAYiTAHMiNOYUN/src/oyuncakma.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class oyuncakma {
+    public static void main(String[] args) {
+        Scanner girdi=new Scanner(System.in);
+        Random rnd=new Random();
+        System.out.println("Sayı tahmin oyunana hoşgeldin");
+        System.out.print("1000 e kadar bir sayı girin:");
+        int sayi=rnd.nextInt(1000);
+        int tahmin= girdi.nextInt();
+        int i=1;
+
+
+        while(sayi !=tahmin){
+
+            if (tahmin<0 || tahmin >1000){
+                System.out.print("Girdiğiniz değer hatalı lütfen 0 ile 1000 aralığında bir sayı giriniz:");
+                i++;
+            } else if (tahmin<sayi) {
+                System.out.print("Daha büyük giriniz:");
+                i++;
+
+
+            }
+            else{
+                System.out.print("Daha küçük giriniz:");
+                i++;
+            }
+            tahmin= girdi.nextInt();
+
+        }
+        System.out.print("Oyunu kazandın tebrikler "+i+" kere denedin.");
+
+
+
+
+
+    }
+}
